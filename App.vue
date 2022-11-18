a9eb124b63d9b7256ae79a60e8498ad429214fbf
Rohan-sinha1209 <61308450+Rohan-sinha1209@users.noreply.github.com>
2022-11-19 03:22:20 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <weatherInfo msg="Weather report of different Places"/>
+</template>
+
+<script>
+import weatherInfo from './components/weatherInfo.vue';
+// import Vue from 'vue';
+// window.axios=require('axios')
+export default {
+  name: 'App',
+  components: {
+    weatherInfo
+  }
+}
+</script>
+
+
